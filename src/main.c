3dc71a3d917bfc589028e515ca31af86664da84a
Seven719 <ilie.andronie@gmail.com>
2024-03-10 12:06:43 +0000

    Made a way to find commands in /usr/bin directory

--- src/main.c
+++ src/main.c
@@ -2,37 +2,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-bool builtIn(char *str) {
-    const char* shell_commands[] = {
-        "cd",
-        "pwd",
-        "echo",
-        "exit",
-        "source",
-        "kill",
-        "man"
-    };
+typedef struct {
+    const char *command;
+    void (*function)();
+} internalCommand;
 
-    int size = sizeof(shell_commands) / sizeof(shell_commands[0]);
+void exitShell();
+void changeDirectory(const char **tokens);
+/* void printWorkingDirectory(); */
+/* void echo(); */
+/* void sourceFile(); */
+/* void killProcess(); */
+/* void showManual(); */
 
-    for(int i = 0; i < size; i++) {
-        if(strcmp(str, shell_commands[i]) == 0) {
-            return true;
-        }
-    }
-    
-    return false;
+void changeDirectory(const char **tokens) {
 }
 
 void exitShell() {
     exit(EXIT_SUCCESS);
 }
 
+bool externalCommand(const char *fname) {
+    const char *directory = "/usr/bin/";
+    char filepath[1033];
+
+    sprintf(filepath, "%s/%s", directory, fname);
+
+    if (access(filepath, F_OK) == 0) {
+        return true;
+    }
+    return false;
+}
+
+void commandHandler(const char *str) {
+    internalCommand builtIn[] = {
+        {"cd", changeDirectory},
+        /* {"pwd", printWorkingDirectory}, */
+        /* {"echo", echo}, */
+        /* {"exit", exitShell}, */
+        /* {"source", sourceFile}, */
+        /* {"kill", killProcess}, */
+        /* {"man", showManual} */
+    };
+   
+    int commandsCount = sizeof(builtIn) / sizeof(builtIn[0]);
+
+    for(int i = 0; i < commandsCount; i++) {
+        if(strcmp(str, builtIn[i].command) == 0) {
+            builtIn[i].function();
+            return;
+        }
+    }
+    
+    if(externalCommand(str)) {
+        printf("Command Exists\n");
+    }
+    else {
+        printf("Command not found: %s\n", str);
+    }
+}
+
 int main(int argc, char *argv[]) {    
-    char *str = NULL, c = '\0';
+    char *str = NULL, *token = NULL, c = '\0';
+    char **tokens = NULL;
 
     int i = 0;
+    int tokenCount = 0;
 
     str = (char*)malloc(sizeof(char));
 
@@ -42,13 +79,17 @@ int main(int argc, char *argv[]) {
         str = realloc(str, (i + 1) * sizeof(char));
         str[i++] = c;
     }
+    
+    tokens = (char**)(malloc(sizeof(char*)));
+    token = strtok(str, " ");
 
-    if(builtIn(str)) {
-        printf("%s is a built in command\n", str);
+    while(token != NULL) {
+        tokens = realloc(tokens, (tokenCount + 1) * sizeof(char*));
+        tokens[tokenCount++] = strndup(token, strlen(token));
+        token = strtok(NULL, " ");
     }
-    else {
-        printf("%s is just random words\n", str);
-    }
-
     free(str);
+
+    commandHandler(tokens[0]);
+    free(tokens);
 }
